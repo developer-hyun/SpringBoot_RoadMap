8c3803d1f2910e4078b31ef9ebb0d654ded5c7ce
developer-hyun <rlckdrkdud@naver.com>
2026-06-09 15:22:04 +0900

    26_06_09_01

--- Spring/Spring_Basic/hello/src/main/java/hello/core/AppConfig.java
+++ Spring/Spring_Basic/hello/src/main/java/hello/core/AppConfig.java
@@ -1,21 +1,45 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     //이렇게 되면 멤버서비스 구현체가 MMR을 직접 연결안하고 기획자인 AppConfig가  MMR을 주입하는식
     //생성자 주입이라고함.
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
-    }
+//    public MemberService memberService() {
+//
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
 
+    //config 안에서도 역할과 구현을 나눔
+    @Bean
+    public MemberService memberService() {
+
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository() , new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+    @Bean
+    public DiscountPolicy discountPolicy() {
+       // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
--- Spring/Spring_Basic/hello/src/main/java/hello/core/MemberApp.java
+++ Spring/Spring_Basic/hello/src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,25 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService= appConfig.memberService();
+       // MemberService memberService = new MemberServiceImpl();
+
+
+        //스프링을 사용하는 버전
+        //스프링컨테이너에넣는다
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //클래스이름 , 타입
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
+
         Member member = new Member(1L,Grade.VIP,"memberA");
         memberService.join(member);
 
